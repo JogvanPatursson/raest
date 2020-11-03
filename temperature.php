3b6bd1154ea1d9935fbad3b58e1ff6e492a7a3ea
sblack-student <2014.062@student.setur.fo>
2020-11-03 15:05:45 +0000

    init

---
+++ temperature.php
@@ -0,0 +1,46 @@
+<?php
+	include_once ('header.php');
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>RAEST Web App</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<style>
+
+	table, th, td, tr {
+		border: 1px solid black;
+	}
+	
+	</style>
+</head>
+<body>
+
+<div align="center">
+	<h1>Temperature</h1>
+	<table>
+		<th>
+			<p>
+				Header1
+			</p>
+		</th>
+				<th>
+			<p>
+				Header2
+			</p>
+		</th>
+		<tr>
+			<td>
+				<p>Data1</p>
+			</td>
+			
+			<td>
+				<p>Data2</p>
+			</td>
+		</tr>
+	</table>
+</div>
+
+</body>
+</html>
