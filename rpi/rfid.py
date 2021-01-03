e1bd4626977ac98ef8f822145edc2bfa39007d82
JogvanPatursson <jogvanpatursson@gmail.com>
2021-01-03 20:56:42 +0000

    Update rfid.py

--- rpi/rfid.py
+++ rpi/rfid.py
@@ -17,7 +17,6 @@ def rfidHandler(rfidInput):
     rfidElapsed = 0
     # Array of string values the user can choose
     number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    success = False
 
     #Set timer before while loop starts
     rfidStart = time.time()
