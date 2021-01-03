615b2251428af347cd8834dc26db4d01e8eaa020
JogvanPatursson <jogvanpatursson@gmail.com>
2021-01-03 21:23:32 +0000

    Update main.py

--- rpi/main.py
+++ rpi/main.py
@@ -40,25 +40,25 @@ if __name__ == '__main__':
     ser.flush()
 
     while True:
+	# Reads serial data from Arduino. Assigns value to string variable line
         line = ser.readline().decode('utf-8').rstrip()
         print(line)
-
-        string = "request"
-        encodedString = string.encode()
-        ser.write(encodedString)
     
-        
+        # If line contains string "RFID: ", remove "RFID: " from line
         if (rfidStr in line):
             rfidInput = line.replace(rfidStr, "")
             led.blueBlink()
             rfid.rfidHandler(rfidInput)
         
+	# If line contains string "T: ", remove "T: " from line
         if (tempStr in line):
             tInput = line.replace(tempStr, "")
 		
+	# If line contains string "H: ", remove "H: " from line
         if (humidStr in line):
             hInput = line.replace(humidStr, "")
-		
+	
+	# If line contains string "MOTION: ", remove "MOTION: " from line
         if (motionStr in line):
             motionInput = line.replace(motionStr, "")
             
@@ -67,9 +67,9 @@ if __name__ == '__main__':
 	    # If more than 60 seconds have passed since last time motionFlag was set to true
             if (elapsed > 60):
                 motionFlag = False
-            
+		
+            # If motionInput is 1, call takePhoto() function, and set motionFlag to true
             if (motionInput == '1'):
-                # Call takePhoto() function, and set motionFlag to true
                 if (motionFlag == False):
                     camera.takePhoto()
                     start = done
@@ -77,15 +77,18 @@ if __name__ == '__main__':
 
         #*********************************************************
         
-        # Send temperature and humidity data to database
+        # Get current minute
         now = datetime.now()
         current_minute = now.strftime("%M")
         current_minute = int(current_minute)
         
+	
         if (dataFlag == False):
+	    # If the current minute is divisible by 10
             if ((current_minute % 10) == 0):
                 dbComm.addData(tInput, hInput)
                 dataFlag = True
+	# If two minutes have passed, set dataFlag to false
         if ((current_minute % 10) == 2):
             dataFlag = False
 
